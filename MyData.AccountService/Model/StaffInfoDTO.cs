aa0f88c19680da58baa6496608033bca4875748f
Apidesh  Wongsa <apidesh@gmail.com>
2020-01-06 15:49:37 +0700

    Added missing dto

---
+++ MyData.AccountService/Model/StaffInfoDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyData.AccountService.Model
+{
+    public class StaffInfoDTO
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public string EmployeeID { get; set; }
+
+        public string DivisionName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string AccountTypeCode { get; set; }
+    }
+}
